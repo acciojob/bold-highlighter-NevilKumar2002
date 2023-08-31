c279ccaa05ea00e13ba7276ca45640b191bcc35d
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2023-08-31 07:39:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+// const strongwords=document.getElementsByTagName("strong");
+const link=document.getElementTagName("a");
+const strongwords=document.getElementsByTagName("strong");
 function highlight() {
     //Write your code here
-
+	for(const words of strongwords){
+        words.style.color='blue';
+    }
 
 }
 
 
 function return_normal() {
     //Write your code here
+	for(const words of strongwords)
+		{
+			strongwords.style.color='black';
+		}
 
     
 }
+// link.addEventListener('onMouseOver',highlight);
+// link.addEventListener('onMouseOut',return_normal)
